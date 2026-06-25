2d87c8fd7f6d310e899f7133ce15a274e35fcecf
mubduster <mubassirmuneeb51@gmail.com>
2026-06-25 17:42:05 +0500

    The first version of this project

---
+++ README.md
@@ -0,0 +1,17 @@
+# My first go game
+## What it is?
+  A basic game made in go so i can learn game development without the need of any fancy game eniges like Unity and Unreal Engine, which all although are good, but it's better to know to code a game than just designing it in my opinion. (add further documentation)
+
+## How to play?
+  Just install the correct version according to your OS, and start playing.
+  ### controls:
+  - W A S D to move
+  - thats it.
+### Ps
+  This version is not the final project, and will get changed. Also I have not figured out a way to make a proper executable file for Windows yet, and I am on linux.
+
+## Author
+  Me! The author of this porject is [mubduster](https://github.com/mubduster/Go).
+
+## License
+  This project is licensed under the MIT License, you may use the source code for this project accordingly to it.
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"fmt"
+)
+
+var speedX float32 = 0
+var speedY float32 = 0
+var shift uint8 = 1
+var hueShift float32 = 0.0
+var dir float32 = 0.4
+var R uint8 = 0
+var G uint8 = 0
+var B uint8 = 0
+var wallX bool
+var wallY bool
+
+func main(){
+	rl.InitWindow(1600, 1020, "Basic Window")
+
+	screenWidth, screenHeight := float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight())
+
+	fmt.Printf("ScreenWidth: %f, ScreenHeight: %f.",screenWidth, screenHeight)
+
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	halfScreenWidth, halfScreenHeight := int32(screenWidth/2), int32(screenHeight/2)
+	
+	x := float32(halfScreenWidth - 200)
+	y := float32(halfScreenHeight - 200)
+	
+	for !rl.WindowShouldClose() {
+		
+		hueShift += dir
+
+		if !rl.IsKeyDown(rl.KeyRight) && !rl.IsKeyDown(rl.KeyLeft) {
+			if speedX > 0 {
+				speedX -= 1
+			}else if speedX < 0 {
+				speedX += 1
+			}
+		}
+		if !rl.IsKeyDown(rl.KeyUp) && !rl.IsKeyDown(rl.KeyDown) {
+			if speedY > 0 {
+				speedY -= 1
+			}else if speedY < 0 {
+				speedY += 1
+			}
+		}
+		if rl.IsKeyDown(rl.KeyUp) && y > 0{
+			speedY -= 1
+		}
+		if rl.IsKeyDown(rl.KeyDown) && y+200 < screenHeight{
+			speedY += 1
+		}
+		if rl.IsKeyDown(rl.KeyRight) && x+300 < screenWidth{
+			speedX += 1
+		}
+		if rl.IsKeyDown(rl.KeyLeft) && x > 0{
+			speedX -= 1
+		}
+
+		R ,G ,B = R+shift, G+shift, B+shift
+		
+		if x+300 > screenWidth {
+			x = screenWidth - 300
+			speedX = speedX * -1 
+		}else if x < 0 {
+			x = 0
+			speedX = speedX * -1
+		}
+		if y+200 > screenHeight{
+			y = screenHeight - 200
+			speedY = speedY * -1
+		}else if y < 0 {
+			y = 0
+			speedY = speedY * -1
+		}
+		
+		x += speedX
+		y += speedY
+		
+		if R >= 225 && G >= 225 && B >= 225 || R <= 0 && G <= 0 && B <= 0 {
+			shift = -shift
+		}
+
+		if hueShift >360.0 {
+			dir = -dir
+		}
+
+		rl.BeginDrawing()
+		
+		rl.ClearBackground(rl.NewColor(R, G, B, 225))
+		rl.DrawRectangle(int32(x), int32(y), 300, 200, rl.ColorFromHSV(hueShift,0.5,0.7))
+		rl.DrawText("Bouncy ractnagle", int32(x)+15, int32(y+80), 30, rl.ColorFromHSV(hueShift/2,1.0,1.0))
+		rl.DrawText(fmt.Sprintf("Screen Width: %0.1f, Screen Height: %0.1f. halfScreenWidth: %0.1f, halfScreenHeight: %0.1f", screenWidth, screenHeight, float32(halfScreenWidth), float32(halfScreenHeight)), 10 , 40, 30, rl.GetColor(0x00FFFF77))
+		rl.DrawText(fmt.Sprintf("Use arrow keys to move the rectangle!"), 10, 90, 30, rl.GetColor(0x00FFFF77))
+		rl.DrawText(fmt.Sprintf("Speed X : %0.1f\nSpeed Y: %0.1f.", speedX, speedY), 10, 120, 30, rl.GetColor(0x00FFFF77))
+		rl.DrawFPS(10, 10)
+		
+		rl.EndDrawing()
+	}	
+}
+
